b60eb8f3e0f2006d5bb32889ee3d38c4ec190ad6
Roberto Pérez <164533998+Roberto-Perez1084@users.noreply.github.com>
2025-02-07 16:48:04 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('Hola mundo');
+}
+
+void escribeNombre(){
+  print('Roberto Pérez Acosta');
+}
+
+void main(){
+  // Roberto Pérez Acosta, Mat: 22308051281084
+  // llamar a la función saludar
+  print('Roberto Pérez Acosta, Mat: 22308051281084');
+  print('Llamando a la función saludar');
+  saludar();
+  print('Llamando a la función escribeNombre');
+  escribeNombre();
+}
