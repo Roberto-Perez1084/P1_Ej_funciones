5102f8951585200741a3caa6e45a4e5e7ebf34b0
Roberto Pérez <164533998+Roberto-Perez1084@users.noreply.github.com>
2025-02-10 18:04:24 +0000

    ejemplo funcion rama

---
+++ funcion2.dart
@@ -0,0 +1,15 @@
+void suma(int a, int b){
+  print('La suma de $a y $b es ${a + b}');
+}
+
+void resta(int a, int b){
+  print('La resta de $a y $b es ${a - b}');
+}
+
+void main(){
+  print('Roberto Pérez Acosta, Mat: 22308051281084');
+  print('Llamando a la función suma');
+  suma(10, 5);
+  print('Llamando a la función resta');
+  resta(10, 5);
+}
